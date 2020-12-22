f29a997a9e484f657fda12561d3d32112a5c2c53
조법룡 <jby0711823@naver.com>
2020-12-22 21:37:29 +0900

    Initial commit

---
+++ src/com/letscodefortest/q1/Q1.java
@@ -0,0 +1,7 @@
+package com.letscodefortest.q1;
+
+public class Q1 {
+    public static void main(String args[]) {
+        System.out.println("hello world1");
+    }
+}
---
+++ src/com/letscodefortest/q2/Q2.java
@@ -0,0 +1,7 @@
+package com.letscodefortest.q2;
+
+public class Q2 {
+    public static void main(String args[]) {
+        System.out.println("hello world2");
+    }
+}
