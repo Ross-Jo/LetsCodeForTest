cda0183e2948ed142fb347a1d1a773beb38df63b
조법룡 <jby0711823@naver.com>
2021-04-18 12:51:28 +0900

    .

--- src/com/letscodefortest/q12/Q12.java
+++ src/com/letscodefortest/q12/Q12.java
@@ -7,17 +7,20 @@ import java.util.ArrayDeque;
 public class Q12 {
     static Solution1 s1 = new Solution1();
 
+    /**
+     * time complexity: O(N)
+     * space complexity: O(N)
+     * Runtime: 41 ms, faster than 23.49% of Java online submissions for Sliding Window Maximum.
+     * Memory Usage: 132.7 MB, less than 5.00% of Java online submissions for Sliding Window Maximum.ry Pickup.
+     * deque에 index를 넣어가면서 최대값을 유지하는 deque을 유지해, 항상 sliding window의 최대값을 산출해내는 방법
+     * deque에 인자를 삽입할 시 초기화 과정 및, 인자 삽입중 deque의 왼쪽에 위치하고 있는 삽입하려는 인자보다 작은 인자들을 deque에서 제거하는게 주요한 포인트.
+     */
     static class Solution1 {
         ArrayDeque<Integer> deq = new ArrayDeque<Integer>();
         int[] nums;
 
         public void clean_deque(int i, int k) {
-            // remove indexes of elements not from sliding window
-            if (!deq.isEmpty() && deq.getFirst() == i - k)
-                deq.removeFirst();
-
-            // remove from deq indexes of all elements
-            // which are smaller than current element nums[i]
+            if (!deq.isEmpty() && deq.getFirst() == i - k) deq.removeFirst();
             while (!deq.isEmpty() && nums[i] > nums[deq.getLast()]) deq.removeLast();
         }
 
@@ -26,19 +29,16 @@ public class Q12 {
             if (n * k == 0) return new int[0];
             if (k == 1) return nums;
 
-            // init deque and output
             this.nums = nums;
             int max_idx = 0;
             for (int i = 0; i < k; i++) {
                 clean_deque(i, k);
                 deq.addLast(i);
-                // compute max in nums[:k]
                 if (nums[i] > nums[max_idx]) max_idx = i;
             }
             int[] output = new int[n - k + 1];
             output[0] = nums[max_idx];
 
-            // build output
             for (int i = k; i < n; i++) {
                 clean_deque(i, k);
                 deq.addLast(i);
