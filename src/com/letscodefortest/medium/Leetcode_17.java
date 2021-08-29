c753449955b7464b14b9400072ec0cc6737eca05
조법룡 <jby0711823@naver.com>
2021-08-30 01:43:17 +0900

    .

--- src/com/letscodefortest/medium/Leetcode_17.java
+++ src/com/letscodefortest/medium/Leetcode_17.java
@@ -42,6 +42,42 @@ public class Leetcode_17 {
         }
     }
 
+    /**
+     * time complexity: O(4^N * N) N: length of digits
+     * space complexity: O(N)
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Letter Combinations of a Phone Number.
+     * Memory Usage: 37.6 MB, less than 86.72% of Java online submissions for Letter Combinations of a Phone Number.
+     * backtrack을 이용해 모든 결과를 조합한다.
+     */
+    static class Solution2 { // solution 참고
+        private List<String> combinations = new ArrayList<>();
+        private Map<Character, String> letters
+                = Map.of('2', "abc", '3', "def", '4', "ghi", '5', "jkl", '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
+        private String phoneDigits;
+
+        public List<String> letterCombinations(String digits) {
+            if (digits.length() == 0) return combinations;
+
+            phoneDigits = digits;
+            backtrack(0, new StringBuilder());
+            return combinations;
+        }
+
+        private void backtrack(int index, StringBuilder path) {
+            if (path.length() == phoneDigits.length()) {
+                combinations.add(path.toString());
+                return;
+            }
+
+            String possibleLetters = letters.get(phoneDigits.charAt(index));
+            for (char letter : possibleLetters.toCharArray()) {
+                path.append(letter);
+                backtrack(index + 1, path);
+                path.deleteCharAt(path.length() - 1);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(s1.letterCombinations("23"));
     }
